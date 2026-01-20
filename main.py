f09d098cd645ccd412bdfb778698a663eedd5916
IamThejus <thejusasokan123@gmail.com>
2026-01-20 21:40:26 +0530

    created regular expressions for select,insert and create

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import re
 
-command_recognition={
-    "select":r"^SELECT\s+(?P<columns>\*|[\w,\s]+)\s+FROM\s+(?P<table>\w+);$",
-    "insert":r"^INSERT\s+INTO\s+(?P<table>\w+)\s+VALUES\s+(?P<value>.+);$"
+command_mapper={
+    "select":[r"^SELECT\s+(?P<columns>\*|[\w,\s]+)\s+FROM\s+(?P<table>\w+);$"],
+    "insert":[r"^INSERT\s+INTO\s+(?P<table>\w+)\s+VALUES\s+(?P<value>.+);$"],
+    "create":{
+        "db":[r"CREATE\s+DB\s+(?P<database>\w+)\s*;$"],
+        "tb":[r"CREATE\s+TB\s+(?P<tabale>\w+)\s+(?P<value>.+);$"],
+    }
 }
 
 
-
-def query_checker(regex,query):
+def query_parser(regex,query):
     return re.match(
         regex,
         query,
         re.IGNORECASE
     )
+# this function works but only for "select" check it
+def query_command_identifier(query,command_mapper):
+    for command in command_mapper:
+        if len(command_mapper[command])==1:
+            if command in query:
+                return command_mapper[command][0]
+
+        else:
+            return query_command_identifier(query,command_mapper[command])
+    return False
 
 while True:
     query=input(">>>")
-    for command in command_recognition:
-        if command in query:
-            result=query_checker(command_recognition[command],query)
-            if result:
-                for element in result.groupdict():
-                    print(element,result[element])
+    cmd=query_command_identifier(query,command_mapper)
+    if cmd:
+        result=query_parser(cmd,query)
+        if result:
+            print(result.groupdict())
+            
